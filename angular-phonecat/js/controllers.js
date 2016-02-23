ef79db21d4626cb5f7488f3ef47d4e0a3623feea
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-02-23 10:51:27 +0000

    google spreadsheet api

--- angular-phonecat/js/controllers.js
+++ angular-phonecat/js/controllers.js
@@ -6,9 +6,13 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone', function($sc
 }]);
 
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone', function($scope, $routeParams, Phone) {
-  $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-    $scope.mainImageUrl = phone.images[0];
-  });
+
+  $scope.phone = Phone.get(
+  	{phoneId: $routeParams.phoneId},
+  	function(phone) {
+    	$scope.mainImageUrl = phone.images[0];
+  	}
+  );
 
   $scope.setImage = function(imageUrl) {
     $scope.mainImageUrl = imageUrl;
